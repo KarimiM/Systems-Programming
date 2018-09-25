58306418a6dba478713a85b48b643f838287e3ea
Stephen Quickel <stephen@quickel.net>
2018-09-25 15:32:27 -0400

    Update mergesort.c

--- CSV Sorter/CSV Sorter/mergesort.c
+++ CSV Sorter/CSV Sorter/mergesort.c
@@ -1,5 +1,5 @@
 //
-//  simplerCSVsorter.h
+//  mergesort.c
 //  CSV Sorter
 //
 //  Created by Masood Karimi on 9/24/18.
@@ -10,15 +10,10 @@
 #define simplerCSVsorter_h
 
 #include <time.h>
-
 #include <stdio.h>
-<<<<<<< HEAD
-=======
+#include "simplerCSVsorter.h"
 
-typedef struct csventry {
-    char** data;
-    
-} csventry;
+
 #endif /* simplerCSVsorter_h */
 
 
@@ -34,11 +29,11 @@ int cmpInt(int firstInteger, int secondInteger){
 	else return 0;
 }
 
-int cmpDateTime(DateTime firstDate, DateTime secondDate ){
-	return 0;
-}
+//int cmpDateTime(DateTime firstDate, DateTime secondDate ){
+//	return 0;
+//}
 
-void merge(csvEntry* entryArr, int low, int mid, int high, int colID){
+void merge(csvEntry* entryArr, int low, int mid, int high, int colID, int numeric){
 	csventry* mergeArr = malloc(sizeof(entryArr));
 	int x = low;
 	int y = mid + 1;
@@ -58,7 +53,7 @@ void merge(csvEntry* entryArr, int low, int mid, int high, int colID){
 			else mergeArr[z++] = entryArr[y++];
 		}
 		else{
-			return 0;
+			return;
 		}
 		while(x <= mid){
 			mergeArr[z++] = entryArr[x++];
@@ -70,21 +65,15 @@ void merge(csvEntry* entryArr, int low, int mid, int high, int colID){
 			entryArr[low + z] = mergeArr[z];
 			z = z-1;
 		}
-
-
-
 	}
-
-
 }
 
-void mergesort(csvEntry* entryArr, int low, int mid, int high, int colID){
+void mergesort(csvEntry* entryArr, int low, int high, int colID, int numeric){
 	if(low<high){
 		int mid = (high+low)/2;
-		mergesort(entryArr, low, mid);
-		mergesort(entryArr, mid+1, high);
-		merge(entryArr, low, mid, high, colID);
+		mergesort(entryArr, low, mid, colID, numeric);
+		mergesort(entryArr, mid+1, high, colID, numeric);
+		merge(entryArr, low, mid, high, colID, numeric);
 	}
 
 }
->>>>>>> 84c16b116eb6520d62a38f4205a50b0e2eb0c9c2
