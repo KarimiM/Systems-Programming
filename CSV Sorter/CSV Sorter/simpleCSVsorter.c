b4cfd40ebefe221c7a5ce61e64b9a0acddfde1df
Stephen Quickel <stephen@quickel.net>
2018-10-01 16:38:08 -0400

    working CSV sorter

--- CSV Sorter/CSV Sorter/simpleCSVsorter.c
+++ CSV Sorter/CSV Sorter/simpleCSVsorter.c
@@ -13,6 +13,7 @@
 #include <time.h>
 #include <sys/stat.h>
 #include "simpleCSVsorter.h"
+#include "mergesort.c"
 
 csventry *columns;
 csventry *entries;
@@ -142,77 +143,9 @@ int cmpInt(int firstInteger, int secondInteger){
 //  return 0;
 //}
 
-void merge(csventry* entryArr, int low, int mid, int high, int colID, int numeric){
-    int i, j, k;
-    int n1 = mid - low + 1;
-    int n2 = high - mid;
-    
-    csventry L[n1];
-    csventry R[n2];
-    
-    for (i = 0; i < n1; i++) {
-        csventry check = entryArr[low + i];
-        //L[i].data = malloc(sizeof(check.data));
-        L[i].data = check.data;
-        L[i].originalIndex = check.originalIndex;
-    }
-    
-    for (j = 0; j < n2; j++) {
-        csventry check = entryArr[mid + 1 + j];
-        //R[j].data = malloc(sizeof(check.data));
-        R[j].data = check.data;
-        R[j].originalIndex = check.originalIndex;
-    }
-    
-    i=0;
-    j=0;
-    k=low;
-    while (i < n1 && j < n2) {
-        if(numeric == 1){
-            int one = atoi(L[i].data[colID]);
-            int two = atoi(R[j].data[colID]);
-            if (one <= two) {
-                entryArr[k] = L[i];
-                i++;
-            } else {
-                entryArr[k] = R[j];
-                j++;
-            }
-        }
-        else if(numeric == 0){
-            char* one = L[i].data[colID];
-            char* two = R[j].data[colID];
-            if(cmpString(one, two) == 1){
-                entryArr[k] = L[i];
-                i++;
-            }
-            else{
-                entryArr[k] = R[j];
-                j++;
-            }
-        }
-        k++;
-    }
-    while (i < n1) {
-        entryArr[k] = L[i];
-        i++;
-        k++;
-    }
-    while (j < n2) {
-        entryArr[k] = R[j];
-        j++;
-        k++;
-    }
-}
 
-void mergesorts(csventry* entryArr, int low, int high, int colID, int numeric){
-    if(low<high){
-        int mid = (high+low)/2;
-        mergesorts(entryArr, low, mid, colID, numeric);
-        mergesorts(entryArr, mid+1, high, colID, numeric);
-        merge(entryArr, low, mid, high, colID, numeric);
-    }
-}
+
+
 
  void printCSV(csventry* entries, int rows, csventry* columnArr){
    // printf("Printing to csv");
@@ -272,7 +205,6 @@ int main(int varc, char* argv[])
         char* buffCopy = malloc(size);
         strncpy(buffCopy, buffer, size);
         if (dataIndex >= dataSize - 100) {
-            puts("reallocing");
             dataSize *= 2;
             originalData = realloc(originalData, dataSize * sizeof(char*));
         }
